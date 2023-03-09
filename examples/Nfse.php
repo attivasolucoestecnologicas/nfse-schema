ca759a976aefe576cc4f2eb6f9f6f947dcc73a99
Walber Jefferson <walberjefferson@hotmail.com>
2023-03-09 14:06:38 -0300

    Ajustes NFS-e

--- examples/Nfse.php
+++ examples/Nfse.php
@@ -12,14 +12,14 @@ $std->Id = '123';
 $std->Numero = '123456'; // NÚMERICO | ATÉ 15 DIGITOS
 $std->CodigoVerificacao = '123'; // CARACTERE | ATÉ 9 DIGITOS
 $std->DataEmissao = '2023-03-07T15:29:00'; //
-$std->NfseSubstituida = null; // NULL | NÚMERICO | ATÉ 15 DIGITOS
-$std->OutrasInformacoes = null; // NULL
-$std->ValorCredito = null; // NULL | EXEMPLO 1000.00
+$std->NfseSubstituida = '123'; // NULL | NÚMERICO | ATÉ 15 DIGITOS
+$std->OutrasInformacoes = "Informações complementares"; // NULL
+$std->ValorCredito = 0.00; // NULL | EXEMPLO 1000.00
 
 $std->ValoresNfse = new stdClass();
-$std->ValoresNfse->BaseCalculo = null;
-$std->ValoresNfse->Aliquota = null;
-$std->ValoresNfse->ValorIss = null;
+$std->ValoresNfse->BaseCalculo = 0.00;
+$std->ValoresNfse->Aliquota = 0.00;
+$std->ValoresNfse->ValorIss = 0.00;
 $std->ValoresNfse->ValorLiquidoNfse = 123435.77;
 
 $std->PrestadorServico = new stdClass();
@@ -28,21 +28,21 @@ $std->PrestadorServico->NomeFantasia = "Armazem";
 
 $std->PrestadorServico->IdentificacaoPrestador = new stdClass();
 $std->PrestadorServico->IdentificacaoPrestador->CpfCnpj = '12345678901';
-$std->PrestadorServico->IdentificacaoPrestador->InscricaoMunicipal = null;
+$std->PrestadorServico->IdentificacaoPrestador->InscricaoMunicipal = '9876543210';
 
 $std->PrestadorServico->Endereco = new stdClass();
 $std->PrestadorServico->Endereco->Endereco = "Rua Teste";
-$std->PrestadorServico->Endereco->Numero = null;
-$std->PrestadorServico->Endereco->Complemento = null;
-$std->PrestadorServico->Endereco->Bairro = null;
-$std->PrestadorServico->Endereco->CodigoMunicipio = null;
-$std->PrestadorServico->Endereco->Uf = null;
-$std->PrestadorServico->Endereco->CodigoPais = null;
-$std->PrestadorServico->Endereco->Cep = null;
+$std->PrestadorServico->Endereco->Numero = '22';
+$std->PrestadorServico->Endereco->Complemento = 'Sem complemento';
+$std->PrestadorServico->Endereco->Bairro = 'Centro';
+$std->PrestadorServico->Endereco->CodigoMunicipio = 0122;
+$std->PrestadorServico->Endereco->Uf = 'AL';
+$std->PrestadorServico->Endereco->CodigoPais = 012;
+$std->PrestadorServico->Endereco->Cep = '55555555';
 
 $std->PrestadorServico->Contato = new stdClass();
 $std->PrestadorServico->Contato->Telefone = "8899999999";
-$std->PrestadorServico->Contato->Email = null;
+$std->PrestadorServico->Contato->Email = 'none@mail.com';
 
 $std->OrgaoGerador = new stdClass();
 $std->OrgaoGerador->CodigoMunicipio = '1234';
@@ -63,7 +63,7 @@ $std->DeclaracaoPrestacaoServico->Rps->RpsSubstituido->Serie = 'ABC';
 $std->DeclaracaoPrestacaoServico->Rps->RpsSubstituido->Tipo = 1;
 
 // tcInfDeclaracaoPrestacaoServico
-$std->DeclaracaoPrestacaoServico->Competencia = '02/2023'; // OK
+$std->DeclaracaoPrestacaoServico->Competencia = '2023-02-09'; // OK
 $std->DeclaracaoPrestacaoServico->Servico = new stdClass();
 $std->DeclaracaoPrestacaoServico->Servico->Valores = new stdClass();
 $std->DeclaracaoPrestacaoServico->Servico->Valores->ValorServicos = 1000.00;
@@ -104,13 +104,13 @@ $std->DeclaracaoPrestacaoServico->TomadorServico->NifTomador = '12345678901';
 $std->DeclaracaoPrestacaoServico->TomadorServico->RazaoSocial = '123456789';
 $std->DeclaracaoPrestacaoServico->TomadorServico->Endereco = new stdClass();
 $std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Endereco = "Rua Teste";
-$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Numero = null;
-$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Complemento = null;
-$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Bairro = null;
-$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->CodigoMunicipio = null;
-$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Uf = null;
-$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->CodigoPais = null;
-$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Cep = null;
+$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Numero = 'SN';
+$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Complemento = 'Sem complemento';
+$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Bairro = 'Centro';
+$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->CodigoMunicipio = 0122;
+$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Uf = 'AL';
+$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->CodigoPais = 012;
+$std->DeclaracaoPrestacaoServico->TomadorServico->Endereco->Cep = '00000000';
 $std->DeclaracaoPrestacaoServico->TomadorServico->Contato = new stdClass();
 $std->DeclaracaoPrestacaoServico->TomadorServico->Contato->Telefone = "8899999999";
 $std->DeclaracaoPrestacaoServico->TomadorServico->Contato->Email = "none@none.com";
@@ -122,11 +122,12 @@ $std->DeclaracaoPrestacaoServico->Intermediario->IdentificacaoIntermediario->Ins
 $std->DeclaracaoPrestacaoServico->ConstrucaoCivil = new stdClass(); // NULL
 $std->DeclaracaoPrestacaoServico->ConstrucaoCivil->CodigoObra = '001'; // NULL
 $std->DeclaracaoPrestacaoServico->ConstrucaoCivil->Art = '111'; // NULL
-$std->DeclaracaoPrestacaoServico->RegimeEspecialTributacao = null; // NULL - OK
+$std->DeclaracaoPrestacaoServico->RegimeEspecialTributacao = 1; // NULL - OK
 $std->DeclaracaoPrestacaoServico->OptanteSimplesNacional = 1; // 1 - SIM | 2 - NÃO - OK
 $std->DeclaracaoPrestacaoServico->IncentivoFiscal = 2;  // 1 - SIM | 2 - NÃO - OK
 $std->DeclaracaoPrestacaoServico->Id = '001'; // OK
 
 header("Content-type: text/xml");
-$nfse = Nfse::save($std, './examples');
+Nfse::save($std, './examples');
+$nfse = Nfse::render($std);
 echo $nfse;
