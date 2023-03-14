a7a426a2c112a63a1873aec40b16763545ffaee7
Walber Jefferson <walberjefferson@hotmail.com>
2023-03-14 18:10:42 -0300

    Montando resposta de substituicao

--- src/Common/Elements/SubstituirNfseResposta.php
+++ src/Common/Elements/SubstituirNfseResposta.php
@@ -4,20 +4,26 @@ namespace Ativasolucoestecnologicas\Nfse\Common\Elements;
 
 use NFePHP\Common\DOMImproved;
 
-class SubstituirNfseEnvio
+class SubstituirNfseResposta
 {
     /**
      * @param \stdClass $std
      * @param DOMImproved $dom
      * @param string $element
      */
-    public static function mount($std, $dom, $element = 'SubstituirNfseEnvio')
+    public static function mount($std, $dom, $element = 'SubstituirNfseResposta')
     {
         $node = $dom->createElement($element);
 
         $node->setAttribute('xmlns', "https://wjdev.net.br/abrasf/v203/nfse.xsd");
 
-        SubstituicaoNfse::mount($std->substituicaonfse, $dom, $node);
+        if ($std->retsubstituicao ?? null) {
+            RetSubstituicao::mount($std->retsubstituicao, $dom, $node);
+        }
+
+        if ($std->listamensagemretorno ?? null) {
+            ListaMensagemRetorno::mount($std->listamensagemretorno, $dom, $node);
+        }
 
         $dom->appendChild($node);
     }
