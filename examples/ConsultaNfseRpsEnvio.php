e4bbbb0b09e39cc173cfe3d1491878d074677d92
Walber Jefferson <walberjefferson@hotmail.com>
2023-03-15 16:34:03 -0300

    Montado servico consulta nfse por rps

---
+++ examples/ConsultaNfseRpsEnvio.php
@@ -0,0 +1,22 @@
+<?php
+
+use Ativasolucoestecnologicas\Nfse\ConsultaNfseRpsEnvio;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$std = new stdClass();
+
+$std->IdentificacaoRps = new stdClass();
+$std->IdentificacaoRps->Numero = '1234';
+$std->IdentificacaoRps->Serie = 'ABC';
+$std->IdentificacaoRps->Tipo = 1;
+
+$std->Prestador = new stdClass();
+$std->Prestador->CpfCnpj = '12345678901234';
+$std->Prestador->InscricaoMunicipal = '12345678';
+
+header("Content-type: text/xml");
+$class = new ConsultaNfseRpsEnvio();
+$class->save($std, 'examples/xml', 'ConsultaNfseRpsEnvio');
--- src/Common/Factory.php
+++ src/Common/Factory.php
@@ -5,12 +5,14 @@ namespace Ativasolucoestecnologicas\Nfse\Common;
 use Ativasolucoestecnologicas\Nfse\Factories\Nfse;
 use Ativasolucoestecnologicas\Nfse\Factories\Request\CancelarNfse as CancelarNfseRequest;
 use Ativasolucoestecnologicas\Nfse\Factories\Request\ConsultaLoteRps as ConsultaLoteRpsRequest;
+use Ativasolucoestecnologicas\Nfse\Factories\Request\ConsultaNfseRps as ConsultaNfseRpsRequest;
 use Ativasolucoestecnologicas\Nfse\Factories\Request\EnviarLoteRps as EnviarLoteRpsRequest;
 use Ativasolucoestecnologicas\Nfse\Factories\Request\EnviarLoteRpsSincrono as EnviarLoteRpsSincronoRequest;
 use Ativasolucoestecnologicas\Nfse\Factories\Request\GerarNfse as GerarNfseRequest;
 use Ativasolucoestecnologicas\Nfse\Factories\Request\SubstituirNfse as SubstituirNfseRequest;
 use Ativasolucoestecnologicas\Nfse\Factories\Response\CancelarNfse as CancelarNfseResponse;
 use Ativasolucoestecnologicas\Nfse\Factories\Response\ConsultaLoteRps as ConsultaLoteRpsResponse;
+use Ativasolucoestecnologicas\Nfse\Factories\Response\ConsultaNfseRps as ConsultaNfseRpsResponse;
 use Ativasolucoestecnologicas\Nfse\Factories\Response\EnviarLoteRps as EnviarLoteRpsResponse;
 use Ativasolucoestecnologicas\Nfse\Factories\Response\EnviarLoteRpsSincrono as EnviarLoteRpsSincronoResponse;
 use Ativasolucoestecnologicas\Nfse\Factories\Response\SubstituirNfse as SubstituirNfseResponse;
@@ -32,6 +34,8 @@ class Factory
     const SUBSTITUIR_NFSE_RESPOSTA = 'SubstituirNfseResponse';
     const CONSULTA_LOTE_RPS_ENVIO = 'ConsultarLoteRpsEnvio';
     const CONSULTA_LOTE_RPS_RESPOSTA = 'ConsultarLoteRpsResposta';
+    const CONSULTA_NFSE_RPS_ENVIO = 'ConsultarNfseRpsEnvio';
+    const CONSULTA_NFSE_RPS_RESPOSTA = 'ConsultarNfseRpsResposta';
 
     public function render($std, $type)
     {
@@ -62,6 +66,10 @@ class Factory
                 return new ConsultaLoteRpsRequest($std);
             case self::CONSULTA_LOTE_RPS_RESPOSTA :
                 return new ConsultaLoteRpsResponse($std);
+            case self::CONSULTA_NFSE_RPS_ENVIO :
+                return new ConsultaNfseRpsRequest($std);
+            case self::CONSULTA_NFSE_RPS_RESPOSTA :
+                return new ConsultaNfseRpsResponse($std);
             default:
                 return null;
         }
